9ee317759c1992d27f9c7526e6ae59844d93898a
Jose Garcia <rocketrito1@gmail.com>
2021-12-14 15:22:05 -0800

    commit tree 85a5fe5b8ef9661b0f39d16773b26290ebf47a18

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (beep) {
+return beep.slice(0,2);
+};
+
+const returnLastTwoDrivers = function (boop) {
+return boop.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+return function (multiplier) {
+return multiplier * integer;
+
+    };
+}
+const fareDoubler = createFareMultiplier(2) ;
+const fareTripler = createFareMultiplier(3) ;
+
+const selectDifferentDrivers = function (drivers, returnFirstTwoDrivers){
+return returnFirstTwoDrivers(drivers);
+};
